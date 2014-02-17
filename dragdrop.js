55c36ddd9bf4220acf5e94c61669c21111078dc5
ewen <ewen.grosjean@gmail.com>
2014-02-17 02:07:58 +0100

    Drag and drop examples

---
+++ dragdrop.js
@@ -0,0 +1,15 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
+goog.addDependency("../schema/utils.js", ['schema.utils'], ['cljs.core', 'goog.string', 'goog.string.format']);
+goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
+goog.addDependency("../schema/core.js", ['schema.core'], ['cljs.core', 'schema.utils', 'clojure.string']);
+goog.addDependency("../domina/support.js", ['domina.support'], ['cljs.core', 'goog.dom', 'goog.events']);
+goog.addDependency("../domina.js", ['domina'], ['goog.dom.classes', 'cljs.core', 'goog.string', 'goog.dom', 'clojure.string', 'goog.dom.xml', 'goog.style', 'goog.dom.forms', 'domina.support', 'goog.events']);
+goog.addDependency("../domina/events.js", ['domina.events'], ['cljs.core', 'goog.object', 'domina', 'goog.events']);
+goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
+goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom']);
+goog.addDependency("../closure-js/libs/flapjax.js", ['F', 'F.dom_', 'F.Behavior'], []);
+goog.addDependency("../ewen/flapjax_cljs.js", ['ewen.flapjax_cljs'], ['cljs.core', 'F.Behavior', 'goog.dom', 'F', 'clojure.string', 'F.dom_']);
+goog.addDependency("../domina/css.js", ['domina.css'], ['cljs.core', 'goog.dom', 'domina', 'goog.dom.query']);
+goog.addDependency("../ewen/dragdrop.js", ['ewen.dragdrop'], ['schema.core', 'cljs.core', 'domina.events', 'om.core', 'ewen.flapjax_cljs', 'domina', 'om.dom', 'goog.style', 'domina.css']);
+goog.addDependency("../ewen/dragdrop/examples.js", ['ewen.dragdrop.examples'], ['schema.core', 'cljs.core', 'domina.events', 'om.core', 'ewen.flapjax_cljs', 'domina', 'om.dom', 'ewen.dragdrop', 'goog.style', 'domina.css']);
