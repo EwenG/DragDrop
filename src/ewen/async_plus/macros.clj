25b829430c2fc4ff42b2f08751199c8c06d55489
ewen <ewen.grosjean@gmail.com>
2014-03-14 19:13:44 +0100

    Remove unused files

--- src/ewen/async_plus/macros.clj
+++ src/ewen/async_plus/macros.clj
@@ -18,16 +18,35 @@
         untaps (cons 'do untaps)]
     `(cljs.core.async.macros/go-loop ~bindings ~@body ~untaps)))
 
-#_(macroexpand-1 '(go-loop [move-ch move-events
-                                 up-ch up-events]
-                                (let [[move-or-up _] (async/alts! [up-ch move-ch]
-                                                                  {:priority true})
-                                      e-type (events/event-type move-or-up)]
-                                  (async/put! out-ch move-or-up)
-                                  (when (= "mousemove" e-type)
-                                    (recur move-ch up-ch)))))
 
-(macroexpand-1
- '(go-loop [gg-ch gg]
-                   (prn (async/<! gg-ch))
-                   (recur gg-ch)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(comment
+  (macroexpand-1 '(go-loop [move-ch move-events
+                            up-ch up-events]
+                           (let [[move-or-up _] (async/alts! [up-ch move-ch]
+                                                             {:priority true})
+                                 e-type (events/event-type move-or-up)]
+                             (async/put! out-ch move-or-up)
+                             (when (= "mousemove" e-type)
+                               (recur move-ch up-ch)))))
+
+  (macroexpand-1
+    '(go-loop [gg-ch gg]
+              (prn (async/<! gg-ch))
+              (recur gg-ch))))
